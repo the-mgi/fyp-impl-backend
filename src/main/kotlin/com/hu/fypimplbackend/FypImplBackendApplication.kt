4a8014959145c560fdc19ec5b85d6e7988b147a2
themgi <the.mgi@outlook.com>
2021-08-17 02:15:11 +0500

    initial commit

---
+++ src/main/kotlin/com/hu/fypimplbackend/FypImplBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.hu.fypimplbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FypImplBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FypImplBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/hu/fypimplbackend/FypImplBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hu.fypimplbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FypImplBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
