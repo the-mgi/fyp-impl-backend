ae6a40f3e654f139004be9da65ec7faaa5ab91c9
themgi <the.mgi@outlook.com>
2021-10-14 03:17:32 +0500

    do not forget to add the EnableEurekaClient

--- src/main/kotlin/com/hu/fypimplbackend/FypImplBackendApplication.kt
+++ src/main/kotlin/com/hu/fypimplbackend/FypImplBackendApplication.kt
@@ -3,11 +3,13 @@ package com.hu.fypimplbackend
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
+@EnableEurekaClient
 class FypImplBackendApplication {
     companion object {
         @JvmStatic
